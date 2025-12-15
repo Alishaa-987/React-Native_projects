f9e6d085d4436903f7867dd45b54cbb66395752e
Alisha Fatima <alishafatima6768@gmail.com>
2025-12-15 23:26:34 +0500

    Add Habit Tracking App

---
+++ Habbit-Tracking-App/Habit-tracking-app/app/(tabs)/add-habbit.tsx
@@ -0,0 +1,99 @@
+import { DATABASE_ID, databases, HABBIT_COLLECTION_ID } from "@/lib/appwrite";
+import { useAuth } from "@/lib/auth-context";
+import { ID } from "appwrite";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, SegmentedButtons, TextInput, useTheme } from "react-native-paper";
+const FREQUENCIES = ["daily", "weekly", "monthly"];
+type Frequency = (typeof FREQUENCIES)[number];
+export default function AddHabbitScreen() {
+  const [frequency, setFrequency] = useState<Frequency>("daily");
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const { user } = useAuth();
+  const router = useRouter();
+  const [error, setError] = useState<string>("");
+  const theme = useTheme();
+
+  const handleSubmit = async () => {
+    if (!user) return;
+    // Here you would typically send the new habit data to your backend or database
+    try {
+      await databases.createDocument(
+         DATABASE_ID,
+         HABBIT_COLLECTION_ID,
+         ID.unique(),
+       {
+        title,
+        description,
+        frequency,
+        user_id: user.$id,
+        streak_count: 0,
+        last_completed: new Date().toISOString(),
+        // created_at: new Date().toISOString(),
+
+      }
+    );
+      router.back();
+    } catch (err) {
+  console.log("Appwrite error:", err);
+  if (err instanceof Error) {
+    setError(err.message); // real message
+  } else {
+    setError("There was an error in creating habbit");
+  }
+}
+
+  
+};
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title} >Add New Habit</Text>
+        <TextInput label="Title" mode="outlined" style={styles.input} value={title} onChangeText={setTitle} />
+        <TextInput label="Description" mode="outlined" style={styles.input} value={description} onChangeText={setDescription} />
+        <View style={styles.frequencyContainer}>
+          <SegmentedButtons
+            value={frequency}
+            onValueChange={(value) => setFrequency(value as Frequency)}
+            buttons={FREQUENCIES.map((freq) => ({
+              value: freq,
+              label: freq.charAt(0).toUpperCase() + freq.slice(1),
+            }))}
+          />
+        </View>
+        <Button mode="contained" style={styles.button} disabled={!title || !description}
+          onPress={handleSubmit}
+        >Add Habit</Button>
+        {error && <Text style={{ color: theme.colors.error }}>{error}</Text>}
+      </View>
+      );
+  }
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+      justifyContent: "center",
+      backgroundColor: "#ffffff",
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: "#2c3e50",
+      textAlign: "center",
+      marginBottom: 30,
+    },
+    input: {
+      marginBottom: 20,
+      backgroundColor: "#f9f9f9",
+    },
+    frequencyContainer: {
+      marginBottom: 30,
+    },
+    button: {
+      marginTop: 20,
+      paddingVertical: 8,
+    },
+  });
---
+++ Habbit-Tracking-App/Habit-tracking-app/lib/auth-context.tsx
@@ -0,0 +1,100 @@
+import { ID, Models } from "appwrite";
+import { createContext, useContext, useEffect, useState } from "react";
+import { account } from "./appwrite";
+
+type AuthContextType = {
+  user: Models.User<Models.Preferences> | null;
+  email: string | null;
+  isLoadingUser: boolean;
+  signUp: (email: string, password: string) => Promise<string | null>;
+  signIn: (email: string, password: string) => Promise<string | null>;
+  signOut: () => Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
+    null
+  );
+
+  const [email, setEmail] = useState<string | null>(null);
+
+  const [isLoadingUser, setIsLoadingUser] = useState<boolean>(true);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const session = await account.get();
+      setUser(session);
+      setEmail(session?.email || null);
+    } catch (error) {
+      setUser(null);
+      setEmail(null);
+    } finally {
+      setIsLoadingUser(false);
+    }
+  };
+
+  const signUp = async (email: string, password: string) => {
+    try {
+      await account.create(ID.unique(), email, password);
+      await signIn(email, password);
+      return null;
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return "An error occured during signup";
+    }
+  };
+  const signIn = async (email: string, password: string) => {
+    if (user) {
+      return "Already logged in";
+    }
+    try {
+      await account.createEmailPasswordSession(email, password);
+      const session = await account.get();
+      setUser(session);
+      setEmail(email);
+      return null;
+    } catch (error) {
+      if (error instanceof Error) {
+        return error.message;
+      }
+
+      return "An error occured during sign in";
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await account.deleteSession("current");
+      setUser(null);
+      setEmail(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, email, isLoadingUser, signUp, signIn, signOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be inside of the AuthProvider");
+  }
+
+  return context;
+}
